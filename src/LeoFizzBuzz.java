c5430156ca517799b4f34bce09994e129cecee4a
Leonardo Mata <leonardo.mata@arpage.com>
2022-03-16 20:36:51 +0100

    Moved valued to constants

--- src/LeoFizzBuzz.java
+++ src/LeoFizzBuzz.java
@@ -1,21 +1,30 @@
 package src;
 
 public class LeoFizzBuzz {
+
+    // first multiple
+    private static final int firstMultiple = 3;
+    private static final String firstKey = "Fizz";
+
+    // second multiple
+    private static final int secondMultiple = 5;
+    private static final String secondKey = "Buzz";
+
     public static void main(String[] args) {
 
         for (int i = 0; i < 100; i++) {
 
-            boolean isMultipleFirst = (i % 3) == 0;
-            boolean isMultipleSecond = (i % 5) == 0;
+            boolean isMultipleFirst = (i % firstMultiple) == 0;
+            boolean isMultipleSecond = (i % secondMultiple) == 0;
 
             String output = null;
 
             if (isMultipleFirst && isMultipleSecond)
-                output = "FizzBuzz";
+                output = firstKey + secondKey;
             else if (isMultipleFirst)
-                output = "Fizz";
+                output = firstKey;
             else if (isMultipleSecond)
-                output = "Buzz";
+                output = secondKey;
             else
                 output = String.valueOf(i);
 
